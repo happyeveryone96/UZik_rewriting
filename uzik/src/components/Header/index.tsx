f5dc61668c39218bc61d494ea2699ecfdb6c8673
happyeveryone96 <66675699+happyeveryone96@users.noreply.github.com>
2021-12-21 21:10:21 +0900

    feat: 헤더 추가

---
+++ uzik/src/components/Header/index.tsx
@@ -0,0 +1,13 @@
+// LIBRARY
+import React from 'react';
+
+// STYLE
+import './index.scss'
+
+const Header = (): React.ReactElement => {
+  return (
+    <div className="header">우리들의 직업</div>
+  )
+}
+
+export default Header;
