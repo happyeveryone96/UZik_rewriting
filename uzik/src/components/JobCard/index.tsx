fa77a12c90d25ba157b9f394585bae769b958a42
happyeveryone96 <66675699+happyeveryone96@users.noreply.github.com>
2021-12-22 15:26:11 +0900

    feat: 스피너 추가

--- uzik/src/components/JobCard/index.tsx
+++ uzik/src/components/JobCard/index.tsx
@@ -1,22 +1,33 @@
 // LIBRARY
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 // STYLE
 import './index.scss'
 
+// COMPONENTS
+import Spinner from "../Spinner";
 
 const JobCard = (props: any): React.ReactElement => {
-  const jobName: string = props.props;
+  const jobName: string = props.props ? props.props : '로딩중';
+  const [isLoaded, setIsLoaded] = useState(true);
+
+  useEffect(() => {
+    if (jobName !== '로딩중') {
+      setIsLoaded(false);
+    }
+  },[jobName])
 
   return (
-    <div className="job">
-      <div className="job-name">{jobName}</div>
-      <a href={`https://search.naver.com/search.naver?query=+${jobName}`} target="_blank" className="job-naver job-search">네이버</a>
-      <a href={`https://search.daum.net/search?q=${jobName}`} target="_blank" className="job-daum job-search">다음</a>
-      <a href={`https://www.google.com/search?q=${jobName}`} target="_blank" className="job-google job-search">구글</a>
-      <a href={`https://www.youtube.com/results?search_query=${jobName}`} target="_blank" className="job-youtube job-search">유튜브</a>
-      <a href={`https://www.work.go.kr/wnSearch/unifSrch.do?topQuery=${jobName}`} target="_blank" className="job-worknet job-search">워크넷</a>
-    </div>
+    <Spinner visible={isLoaded}>
+      <div className="job">
+        <div className="job-name">{jobName}</div>
+        <a href={`https://search.naver.com/search.naver?query=+${jobName}`} target="_blank" className="job-naver job-search">네이버</a>
+        <a href={`https://search.daum.net/search?q=${jobName}`} target="_blank" className="job-daum job-search">다음</a>
+        <a href={`https://www.google.com/search?q=${jobName}`} target="_blank" className="job-google job-search">구글</a>
+        <a href={`https://www.youtube.com/results?search_query=${jobName}`} target="_blank" className="job-youtube job-search">유튜브</a>
+        <a href={`https://www.work.go.kr/wnSearch/unifSrch.do?topQuery=${jobName}`} target="_blank" className="job-worknet job-search">워크넷</a>
+      </div>
+    </Spinner>
   )
 }
 
---
+++ uzik/src/components/Spinner/index.tsx
@@ -0,0 +1,29 @@
+// LIBRARY
+import React from "react";
+import { FadeLoader } from "react-spinners";
+import PropTypes from "prop-types";
+
+// STYLE
+import './index.scss'
+
+const Spinner = (props: any) => {
+  const visible = props.visible;
+
+  return (
+    <>
+      {visible ? (
+        <div className="spinner">
+          <FadeLoader color="rgba(251,216,134,1);" />
+        </div>
+      ) : (
+        <>{props.children}</>
+      )}
+    </>
+  );
+};
+
+// Spinner.propTypes = {
+//   visible: PropTypes.bool,
+// };
+
+export default Spinner;
