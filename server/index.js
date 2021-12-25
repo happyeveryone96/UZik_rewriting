421580bd94d0c43d83916a1738be7123315e60a2
happyeveryone96 <66675699+happyeveryone96@users.noreply.github.com>
2021-12-25 11:05:20 +0900

    feat: 로그인 기능 추가

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const app = express()
 const port = 5000
 const config = require('./config/key');
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 const { User } = require('./models/User');
 
 // application/x-www-form-urlencoded
@@ -12,6 +13,7 @@ app.use(bodyParser.urlencoded({
 
 // application/json
 app.use(bodyParser.json());
+app.use(cookieParser());
 
 const mongoose = require('mongoose')
 mongoose.connect(config.mongoURI).then(() => console.log('MongoDB Connected...'))
@@ -30,6 +32,39 @@ app.post('/register', (req, res) => {
     })
     return res.status(200).json({
       success: true
+    })
+  })
+})
+
+app.post('/login', (req, res) => {
+  // 요청된 이메일을 데이터베이스에서 있는지 찾는다.
+  User.findOne({
+    email: req.body.email
+  }, (err, user) => {
+    if (!user) {
+      return res.json({
+        loginSuccess: false,
+        message: '제공된 이메일에 해당하는 유저가 없습니다.'
+      })
+    }
+    // 요청된 이메일이 데이터 베이스에 있다면 비밀번호가 맞는 비밀번호인지 확인.
+    user.comparePassword(req.body.password, (err, isMatch) => {
+      if (!isMatch)
+        return res.json({
+          loginSuccess: false,
+          message: '비밀번호가 틀렸습니다.'
+        })
+    })
+    // 비밀번호까지 맞다면 토큰을 생성하기.
+    user.generateToken((err, user) => {
+      if (err) return res.status(400).send(err);
+      // 쿠키에 토큰을 저장한다.
+      res.cookie('x_auth', user.token)
+        .status(200)
+        .json({
+          loginSuccess: true,
+          userId: user._id
+        })
     })
   })
 })
