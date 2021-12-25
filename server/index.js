58cd18ef3bd8c1c0aa11289e7da5435fc9fbe327
happyeveryone96 <66675699+happyeveryone96@users.noreply.github.com>
2021-12-25 11:16:05 +0900

    feat: Auth 기능 추가

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ const port = 5000
 const config = require('./config/key');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
+const { auth } = require('./middleware/auth');
 const { User } = require('./models/User');
 
 // application/x-www-form-urlencoded
@@ -23,7 +24,7 @@ app.get('/', (req, res) => {
   res.send('Hello!')
 })
 
-app.post('/register', (req, res) => {
+app.post('api/user/register', (req, res) => {
   const user = new User(req.body);
   user.save((err, userInfo) => {
     if (err) return res.json({
@@ -36,7 +37,7 @@ app.post('/register', (req, res) => {
   })
 })
 
-app.post('/login', (req, res) => {
+app.post('api/user/login', (req, res) => {
   // 요청된 이메일을 데이터베이스에서 있는지 찾는다.
   User.findOne({
     email: req.body.email
@@ -66,6 +67,20 @@ app.post('/login', (req, res) => {
           userId: user._id
         })
     })
+  })
+})
+
+app.get('/api/user/auth', auth, (req, res) => {
+  // 여기까지 미들웨어를 통과해 왔다는 얘기는 Authentication이 True라는 말
+  res.status(200).json({
+    _id: req.user._id,
+    isAdmin: req.user.role === 0 ? false : true,
+    isAuth: true,
+    email: req.user.email,
+    name: req.user.name,
+    lastname: req.user.lastname,
+    role: req.user.role,
+    image: req.user.image,
   })
 })
 
