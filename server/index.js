65629283843faa88e1b8b7421a59bf8d37729875
happyeveryone96 <66675699+happyeveryone96@users.noreply.github.com>
2021-12-25 10:36:11 +0900

    feat: 회원가입 기능 추가

--- server/index.js
+++ server/index.js
@@ -2,6 +2,16 @@ const express = require('express')
 const app = express()
 const port = 5000
 const config = require('./config/key');
+const bodyParser = require('body-parser');
+const { User } = require('./models/User');
+
+// application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+// application/json
+app.use(bodyParser.json());
 
 const mongoose = require('mongoose')
 mongoose.connect(config.mongoURI).then(() => console.log('MongoDB Connected...'))
@@ -9,6 +19,19 @@ mongoose.connect(config.mongoURI).then(() => console.log('MongoDB Connected...')
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+app.post('/register', (req, res) => {
+  const user = new User(req.body);
+  user.save((err, userInfo) => {
+    if (err) return res.json({
+      success: false,
+      err
+    })
+    return res.status(200).json({
+      success: true
+    })
+  })
 })
 
 app.listen(port, () => {
