c2d8eeccb0b03821ba61246f36735a66ae4eee19
happyeveryone96 <66675699+happyeveryone96@users.noreply.github.com>
2021-12-25 11:27:38 +0900

    feat: 라우팅 설정

--- client/uzik/src/App.tsx
+++ client/uzik/src/App.tsx
@@ -5,10 +5,13 @@ import React from 'react';
 import './App.scss';
 
 // COMPONENTS
-import Header from './components/Header'
+import Header from './components/Header';
 
 // PAGES
 import Home from './pages/Home';
+import LoginPage from './pages/Login';
+import RegisterPage from './pages/Register';
+
 
 // ROUTE
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
@@ -20,6 +23,8 @@ function App() {
         <Header/>
           <Routes>
             <Route path='/' element={<Home />}/>
+            <Route path='/login' element={<LoginPage />}/>
+            <Route path='/register' element={<RegisterPage />}/>
           </Routes>
       </div>
     </BrowserRouter>
