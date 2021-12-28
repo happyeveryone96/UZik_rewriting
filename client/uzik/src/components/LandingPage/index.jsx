9c62787912d12b1dd224d07cfebae53ea5c0c856
happyeveryone96 <66675699+happyeveryone96@users.noreply.github.com>
2021-12-29 00:48:06 +0900

    feat: 오픈 api 연결

--- client/uzik/src/components/LandingPage/index.jsx
+++ client/uzik/src/components/LandingPage/index.jsx
@@ -4,42 +4,36 @@ import React, { useEffect } from 'react';
 // AXIOS
 import axios from 'axios';
 
-// ROUTE
-import { useNavigate } from 'react-router-dom';
-
 // COMPONENTS
 import JobCard from '../../components/JobCard';
 import Header from '../../components/Header';
 
 // REDUX
 import { useDispatch, useSelector } from 'react-redux';
+import { getAllJobsApi } from '../../redux/modules/job'
 
 function LandingPage() {
-  const navigate = useNavigate();
   const dispatch = useDispatch()
 
-  // const allJobs = useSelector((state: RootState) => state.job.job_list);
-  // const regex = /[^0-9]/g;
-  // const newAllJobs = allJobs.filter(job => regex.test(job) === true);
-  // let randomIndexArray = [];
+  const allJobs = useSelector((state) => state.job.job_list);
+  const regex = /[^0-9]/g;
+  const newAllJobs = allJobs.filter(job => regex.test(job) === true);
+  let randomIndexArray = [];
 
-  // for (let i=0; i<3; i++) {
-  //   const randomNum = Math.floor(Math.random() * 328);
-  //   if (randomIndexArray.indexOf(randomNum) === -1) {
-  //     randomIndexArray.push(randomNum)
-  //   }
-  // }
+  for (let i=0; i<3; i++) {
+    const randomNum = Math.floor(Math.random() * 328);
+    if (randomIndexArray.indexOf(randomNum) === -1) {
+      randomIndexArray.push(randomNum)
+    }
+  }
 
-  // const firstJob: string = newAllJobs[randomIndexArray[0]];
-  // const secondJob: string = newAllJobs[randomIndexArray[1]];
-  // const thirdJob: string = newAllJobs[randomIndexArray[2]];
+  const firstJob = newAllJobs[randomIndexArray[0]];
+  const secondJob = newAllJobs[randomIndexArray[1]];
+  const thirdJob = newAllJobs[randomIndexArray[2]];
 
-  // useEffect(() => {
-  //   dispatch(getAllJobsApi());
-  // },[dispatch]);
-  const firstJob = '축구선수';
-  const secondJob = '축구선수';
-  const thirdJob = '축구선수';
+  useEffect(() => {
+    dispatch(getAllJobsApi());
+  },[dispatch]);
 
   useEffect(() => {
     axios.get('/api/hello')
@@ -58,4 +52,4 @@ function LandingPage() {
   )
 }
 
-export default LandingPage
+export default LandingPage;
