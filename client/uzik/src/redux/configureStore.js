bedb412fd164f4f49add4932a742a6baa011859c
happyeveryone96 <66675699+happyeveryone96@users.noreply.github.com>
2022-01-01 23:17:04 +0900

    refactor: 불필요한 코드 정리

--- client/uzik/src/redux/configureStore.js
+++ client/uzik/src/redux/configureStore.js
@@ -2,8 +2,8 @@ import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import { createBrowserHistory } from 'history';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
-import job from './modules/job';
 import { connectRouter } from 'connected-react-router';
+import job from './modules/job';
 import user from './modules/user.ts';
 import post from './modules/post';
 
--- client/uzik/src/shared/api.ts
+++ client/uzik/src/shared/api.ts
@@ -2,18 +2,12 @@
 // Api Types
 import { SignInType, SignUpType } from './ApiTypes';
 
-// Info
-import { setUserInfo } from './userInfo';
-
-// redux
-import { history } from '../redux/configureStore';
-
 // axios
 import axios from 'axios';
 
 // token / jwt
-import { getToken, delToken } from './token';
-import jwtDecode from 'jwt-decode';
+import { getToken } from './token';
+
 export const instance = axios.create({
   // baseURL: 'http://13.125.2.115:8080/',
   baseURL: 'http://localhost:8080/',
@@ -28,28 +22,6 @@ instance.interceptors.request.use((config) => {
   config.headers!.Accept = 'application/json';
   return config;
 });
-
-instance.interceptors.response.use(
-  (response) => {
-    return Promise.resolve(response);
-  },
-  (err) => {
-    const path = window.location.pathname;
-
-    if (err.response.status === 307) {
-      setUserInfo('userInfo', jwtDecode(getToken()));
-      history.go(0);
-    } else if (
-      err.response.status === 401 &&
-      !['/register', '/'].includes(path)
-    ) {
-      window.alert('토큰이 만료되었어요! 다시 로그인해주세요!');
-      delToken();
-      history.push('/');
-    }
-    return Promise.reject(err);
-  }
-);
 
 const apis = {
   SignUp: (user: SignUpType) => axios.post('/api/user/register', user),
